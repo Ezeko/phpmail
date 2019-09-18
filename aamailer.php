1291a7571a9194387371f7e053517affb91d4576
Adejobi Ezekiel <31517324+Ezeko@users.noreply.github.com>
2019-09-18 01:17:12 +0100

    Create aamailer.php
    php mailer

---
+++ aamailer.php
@@ -0,0 +1,37 @@
+<?php
+
+  $email= $_REQUEST['email'];
+  $firstname = $_REQUEST['firstname'];
+  $lastname = $_REQUEST['lastname'];
+  $ticketID = $_REQUEST['ticketID'];
+  $team = "ADMIN";
+  $to = $email;
+  $headers  = "MIME-Version: 1.0" . "\r\n";
+  $headers .= "Content-type: text/html; charset=iso-8859-1" . "\r\n";
+  $headers  .= "From: NO-REPLY<no-reply@mydomain.com>" . "\r\n";
+  $subject = $_REQUEST['subject'];
+  $message = '<html>
+                  <body>
+                      <p style="color: green">Hi '.$firstname.' '.$lastname.'</p>
+                      <p>
+                          We recieved below details from you. Please use given Request/Ticket ID for future follow up:
+                      </p>
+                      <p>
+                          Your Request/Ticket ID: <b>'.$ticketID.'</b>
+                      </p>
+                      <p>
+                      Thanks,<br>
+                      '.$team.' Team.
+                      </p>
+                  </body>
+              </html>';
+  $success = mail( $to, $subject, $message, $headers ); 
+  
+  if( $success == true){
+  echo ("Email sent to $email successfully!!!");
+  }else{
+  
+ echo "<script>alert('COULD NOT SEND EMAIL TO $email!!!'); window.location.replace('automailsender.php');</script>";
+
+  }
+?>
